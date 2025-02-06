91dab7782af21d6789a5182056c5ee0d4c602af0
danilanomad <allod2202@gmail.com>
2025-02-06 23:57:45 +0300

    Initial commit of game project

--- bin/gcd.php
+++ bin/gcd.php
@@ -1,7 +1,14 @@
 #!/usr/bin/env php
 <?php
 
-require_once __DIR__ . '\..\vendor\autoload.php';
+$loadPath1 = __DIR__ . '/../../../autoload.php';
+$loadPath2 = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($loadPath1)) {
+    require_once $loadPath1;
+} else {
+    require_once $loadPath2;
+}
 use danilanomad\GCD\Controller;
 
 $controller = new danilanomad\GCD\Controller();
